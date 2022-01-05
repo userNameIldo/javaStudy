ca088aa7be02156d55ee4e0858dfb8c4213ff738
Ildo Kim <ildo5689@naver.com>
2022-01-05 17:33:10 +0900

    study reference type String new String, hashcode(), identityHashCode(), check same

--- src/exam1/referenceType.java
+++ src/exam1/referenceType.java
@@ -4,7 +4,30 @@ public class referenceType {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("check Git git hhhh");
+		
+		String str1 = "hello";
+		String str2 = "hello";
+		
+		String str3 = new String("hello");
+		String str4 = new String("hello"); //객체 생성 연산자는 힙영역에 따로 객체를 생성하여 주소값이 다르다
+		
+		System.out.println("str1==str2 : " + (str1==str2));
+		System.out.println("str1.equals(str2) : " + str1.equals(str2));
+		System.out.println("str3==str2 : "+ (str3==str2));
+		System.out.println("str3.equals(str2) : " + str3.equals(str2));
+		System.out.println("str3==str4 : "+ (str3==str4));
+		System.out.println("str3.equals(str4) : " + str3.equals(str4));
+		System.out.println("str1 : " + str1.hashCode());
+		System.out.println("str2 : " + str2.hashCode());
+		System.out.println("str3 : " + str3.hashCode());
+		System.out.println("str4 : " + str4.hashCode());
+		System.out.println("str1 : " + System.identityHashCode(str1));
+		System.out.println("str2 : " + System.identityHashCode(str2));
+		System.out.println("str3 : " + System.identityHashCode(str3));
+		System.out.println("str4 : " + System.identityHashCode(str4));
+
+		
+		
 	}
 
 }
