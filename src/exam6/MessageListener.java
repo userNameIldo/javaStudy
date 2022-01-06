cf93a8b7113cea1beaba1ab00da37f6e10fd82d9
userNameIldo <ildo5689@naver.com>
2022-01-06 21:22:08 +0900

    nested interface

--- src/exam6/MessageListener.java
+++ src/exam6/MessageListener.java
@@ -6,7 +6,7 @@ public class MessageListener implements Button.OnClickListener{
 	public void onClick() {//메소드 오버라이딩
 		// TODO Auto-generated method stub
 		
-		//구현 객체
+		
 		System.out.println("메시지를 보냅니다.");
 	}
 
