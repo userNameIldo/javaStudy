cfc8a895884c1982298a13fe2c406ee7b505ddf6
userNameIldo <ildo5896@naver.com>
2022-01-06 21:23:49 +0900

    nested interface

--- src/exam6/CallListener.java
+++ src/exam6/CallListener.java
@@ -6,7 +6,6 @@ public class CallListener implements Button.OnClickListener{
 	public void onClick() {//메소드 오버라이딩
 		// TODO Auto-generated method stub
 		
-		
 		System.out.println("전화를 겁니다.");
 		
 	}
--- src/exam6/MessageListener.java
+++ src/exam6/MessageListener.java
@@ -6,7 +6,6 @@ public class MessageListener implements Button.OnClickListener{
 	public void onClick() {//메소드 오버라이딩
 		// TODO Auto-generated method stub
 		
-		
 		System.out.println("메시지를 보냅니다.");
 	}
 
