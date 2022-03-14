4a6ef28865924a7c9027bb680e23aedcc306f821
김일도 <@DESKTOP-HO5EJIK>
2022-03-14 19:29:54 +0900

    function interface

---
+++ src/Lamda/Math.java
@@ -0,0 +1,5 @@
+package Lamda;
+
+public interface Math {
+	public int Calc(int num1, int num2);
+}
---
+++ src/Lamda/test1.java
@@ -0,0 +1,23 @@
+package Lamda;
+
+import java.util.function.IntFunction;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		Math plusCalc = (num1, num2) -> num1 + num2;
+		
+		System.out.println(plusCalc.Calc(5, 6));
+		
+		Math manusCalc = (num1, num2) -> num1 - num2;
+		
+		System.out.println(manusCalc.Calc(7, 2));
+
+		IntFunction intSum = x -> x+1;
+		
+		System.out.println(intSum.apply(6));
+		
+	}
+
+}
